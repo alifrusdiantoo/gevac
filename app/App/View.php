917c852f4794de20051fc4b8931400f2abc85c10
Alif Rusdianto <alifrusdianto30@gmail.com>
2024-08-07 19:46:35 +0700

    feat(App): add router and view

---
+++ app/App/View.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Rusdianto\Gevac\App;
+
+class View
+{
+    public static function render(string $view, $model): void
+    {
+        require __DIR__ . "/../View/header.php";
+        require __DIR__ . "/../View/" . $view . ".php";
+        require __DIR__ . "/../View/footer.php";
+    }
+
+    public static function redirect(string $url)
+    {
+        header("Location: $url");
+        exit();
+    }
+}
