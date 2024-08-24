a609fa76d884f849c4aa16dcc406299a17cf8eaa
Alif Rusdianto <alifrusdianto30@gmail.com>
2024-08-24 12:38:37 +0700

    feat: add super admin only middleware

--- app/Middleware/SupAdminOnlyMiddleware.php
+++ app/Middleware/SupAdminOnlyMiddleware.php
@@ -3,14 +3,13 @@
 namespace Rusdianto\Gevac\Middleware;
 
 use Rusdianto\Gevac\App\View;
+use Rusdianto\Gevac\Domain\Role;
 use Rusdianto\Gevac\Config\Database;
-use Rusdianto\Gevac\Middleware\Middleware;
 use Rusdianto\Gevac\Service\SessionService;
 use Rusdianto\Gevac\Repository\UserRepository;
 use Rusdianto\Gevac\Repository\SessionRepository;
 
-
-class MustNotLoginMiddleware implements Middleware
+class SupAdminOnlyMiddleware implements Middleware
 {
     private SessionService $sessionService;
 
@@ -22,11 +21,11 @@ class MustNotLoginMiddleware implements Middleware
         $this->sessionService = new SessionService($sessionRepository, $userRepository);
     }
 
-    function before(): void
+    public function before(): void
     {
         $user = $this->sessionService->current();
-        if ($user != null) {
-            View::redirect('/overview');
+        if ($user->getRoles() !== Role::from("sup-admin")) {
+            View::redirect("/overview");
         }
     }
 }
--- public/index.php
+++ public/index.php
@@ -7,6 +7,7 @@ use Rusdianto\Gevac\Controller\UserController;
 use Rusdianto\Gevac\Controller\PesertaController;
 use Rusdianto\Gevac\Middleware\MustLoginMiddleware;
 use Rusdianto\Gevac\Middleware\MustNotLoginMiddleware;
+use Rusdianto\Gevac\Middleware\SupAdminOnlyMiddleware;
 
 Database::getConnection("prod");
 
@@ -19,13 +20,13 @@ Router::add("GET", "/peserta", PesertaController::class, "index", [MustLoginMidd
 Router::add("GET", "/", UserController::class, "login", [MustNotLoginMiddleware::class]);
 Router::add("POST", "/", UserController::class, "postLogin", [MustNotLoginMiddleware::class]);
 Router::add("GET", "/logout", UserController::class, "logout", [MustLoginMiddleware::class]);
-Router::add("GET", "/users", UserController::class, "index", [MustLoginMiddleware::class]);
-Router::add("GET", "/users/register", UserController::class, "register", [MustLoginMiddleware::class]);
-Router::add("POST", "/users/register", UserController::class, "postRegister", [MustLoginMiddleware::class]);
-Router::add("POST", "/users/{id}", UserController::class, "delete", [MustLoginMiddleware::class]);
-Router::add("GET", "/users/edit/{id}", UserController::class, "updateProfile", [MustLoginMiddleware::class]);
-Router::add("POST", "/users/edit/{id}", UserController::class, "postUpdateProfile", [MustLoginMiddleware::class]);
-Router::add("GET", "/users/password/{id}", UserController::class, "updatePassword", [MustLoginMiddleware::class]);
-Router::add("POST", "/users/password/{id}", UserController::class, "postUpdatePassword", [MustLoginMiddleware::class]);
+Router::add("GET", "/users", UserController::class, "index", [MustLoginMiddleware::class, SupAdminOnlyMiddleware::class]);
+Router::add("GET", "/users/register", UserController::class, "register", [MustLoginMiddleware::class, SupAdminOnlyMiddleware::class]);
+Router::add("POST", "/users/register", UserController::class, "postRegister", [MustLoginMiddleware::class, SupAdminOnlyMiddleware::class]);
+Router::add("POST", "/users/{id}", UserController::class, "delete", [MustLoginMiddleware::class, SupAdminOnlyMiddleware::class]);
+Router::add("GET", "/users/edit/{id}", UserController::class, "updateProfile", [MustLoginMiddleware::class, SupAdminOnlyMiddleware::class]);
+Router::add("POST", "/users/edit/{id}", UserController::class, "postUpdateProfile", [MustLoginMiddleware::class, SupAdminOnlyMiddleware::class]);
+Router::add("GET", "/users/password/{id}", UserController::class, "updatePassword", [MustLoginMiddleware::class, SupAdminOnlyMiddleware::class]);
+Router::add("POST", "/users/password/{id}", UserController::class, "postUpdatePassword", [MustLoginMiddleware::class, SupAdminOnlyMiddleware::class]);
 
 Router::run();
